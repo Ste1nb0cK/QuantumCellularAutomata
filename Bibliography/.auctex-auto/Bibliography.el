4643a8b90375d8d2cb10d419cc16e19144fe4400
Nicolás A. Niño <nicolas0nn7@gmail.com>
2022-10-21 20:32:50 -0400

    Initial comit

---
+++ Bibliography/.auctex-auto/Bibliography.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "Bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "palpacelli2007ground"
+    "colmenares2009numerical"
+    "succi1996numerical"
+    "palpacelli2007numerical"
+    "palpacelli2009quantum"
+    "palpacelli2008quantum"
+    "EduardoOrtega2004"
+    "IvanPulido2015"))
+ :bibtex)
+
